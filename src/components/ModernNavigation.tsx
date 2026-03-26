2eee9f4456d0b11f2b4b9efa0fdfca13bf5a54b6
Md Abdulla Al Mamun <socalbirdit@gmail.com0>
2026-03-26 22:20:25 +0600

    Fix header container width to match body max-w-7xl

--- src/components/ModernNavigation.tsx
+++ src/components/ModernNavigation.tsx
@@ -70,7 +70,7 @@ export function ModernNavigation({ currentPage, onNavigate }: ModernNavigationPr
         : 'bg-transparent'
         }`}
     >
-      <div className="container mx-auto px-4 lg:px-8">
+      <div className="container mx-auto max-w-7xl px-4">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <button
